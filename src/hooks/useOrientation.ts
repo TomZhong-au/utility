d7bfb2ea6e9b1ed2329c497b0d6bc8c850333a77
Tom <tomzhongcn@gmail.com>
2023-04-23 11:45:01 +0930

    feat: add remove eventlistner in custome hooks

--- src/hooks/useOrientation.ts
+++ src/hooks/useOrientation.ts
@@ -1,16 +1,28 @@
 import { useEffect, useState } from "react";
 
 export default function useOrientation() {
-  const [orientation, setOrientation] = useState<"portrait" | "landscape">("portrait");
+  const [orientation, setOrientation] = useState<"portrait" | "landscape">(
+    "portrait"
+  );
 
   useEffect(() => {
-    if (screen.orientation) {
-      screen.orientation.addEventListener("change", () => {
+    const getOrientation = () => {
+      if (screen.orientation) {
         screen.orientation.type.includes("portrait")
           ? setOrientation("portrait")
           : setOrientation("landscape");
-      });
+      }
+    };
+
+    if (screen.orientation) {
+      screen.orientation.addEventListener("change", getOrientation);
     }
+
+    return () => {
+      if (screen.orientation) {
+        screen.orientation.removeEventListener("change", getOrientation);
+      }
+    };
   }, []);
 
   return orientation;
